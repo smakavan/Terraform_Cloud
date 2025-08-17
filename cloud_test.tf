27ec5d3de5a0ce9282722a6ecf0c5df621325c7b
smakavan <sachinmakavana@gmail.com>
2025-08-17 17:56:34 +0530

    cloud_test.tf

--- cloud_test.tf
+++ cloud_test.tf
@@ -5,12 +5,14 @@ terraform {
     }
   }
 }
+
+# here i have modified secrets
 provider "azurerm" {
   features {}
-  client_id       = "8fc21970-be2a-446d-8f66-67d88c7f2b56"
-  client_secret   = "s4G8Q~ReZ4v_mLdaH7h_XuygVzJ3cYpTIarILai6"
-  tenant_id       = "c761b29e-6ae0-4b0e-ac0b-cb030b4b4b32"
-  subscription_id = "5fd92075-bce6-4a41-ba79-266a440ff078"
+  client_id       = "8fc21970-be2a-446d-8f66-67d88c7f2b50"
+  client_secret   = "s4G8Q~ReZ4v_mLdaH7h_XuygVzJ3cYpTIarILai0"
+  tenant_id       = "c761b29e-6ae0-4b0e-ac0b-cb030b4b4b30"
+  subscription_id = "5fd92075-bce6-4a41-ba79-266a440ff070"
 }
 
 
