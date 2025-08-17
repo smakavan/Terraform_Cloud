c499118f00fbeedfb6c558d796ac709a3dcbc7ee
smakavan <sachinmakavana@gmail.com>
2025-08-17 19:11:02 +0530

    variable.tf

--- WindowsVM/variable.tf
+++ WindowsVM/variable.tf
@@ -72,3 +72,28 @@ variable "vm_size" {
 variable "pass_win" {
 	default = "Pass@1234567"
 }
+
+
+# ---- variable for credentials ----
+
+variable "client_id" {
+  description = "Azure service principal client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure service principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
